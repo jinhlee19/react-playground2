64ae9fe9ce2a6513ef73a0328cd0b8ebb6256a6d
rafy <jinhlee19@gmail.com>
2021-11-29 23:27:55 +0900

    Keeper App Part II

--- keeper-app/src/component/App.jsx
+++ keeper-app/src/component/App.jsx
@@ -2,13 +2,30 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes.js"; 
+console.log(notes);
+
+// #1 app.jsx의 prop 
+// #2-1 prop setting at Note.jsx
+// #2-2 notes.js 연결 export-> import 
+// #3 app.jsx에 createNote() 추가 및 map function 적용
+// #4 arrow function 으로 바꿔주기
+// function단어, return은 단수<>이기 때문에 삭제, 
+// => ()로 묶어주는건 보기편하라는 convention
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<Note />
-			<Footer />
+			{/* {notes.map(createNotes)} */}
+			{notes.map( noteItem => (
+			<Note
+				key={noteItem.key}
+				title={noteItem.title}
+				content={noteItem.content}
+			/>
+		))} 
+		<Footer />
 		</div>
 	);
 }
--- map-filter-reduce/src/index.js
+++ map-filter-reduce/src/index.js
@@ -1,69 +1,81 @@
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
+//// map이라는 function 내부에 새로운 function을 요구하는 형태
+function double(x) {
+  return x * 2;
+}
 
-// function double(x) {
-//   return x * 2;
-// }
-// const newNumbers = numbers.map(double);
+//// map #2
+const newNumbers = numbers.map(function (x) {
+  return x * 2;
+});
 
-// var newNumbers = [];
-// numbers.forEach(function (x) {
-//   newNumbers.push(x * 2);
-// });
+//// foreach #1
+var newNumbers = [];
+function double(x) {
+  newNumbers.push(x * 2);
+}
+numbers.forEach(double);
 
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
-
-// console.log(newNumbers);
+//// forEach #2
+var newNumbers = [];
+numbers.forEach(function (x) {
+  newNumbers.push(x * 2);
+});
 
 //Filter - Create a new array by keeping the items that return true.
 
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+//// 필터 적용
+const newNumbers = numbers.filter(function (num) {
+  return num > 10;
+});
 
-// var newNumbers = [];
-// numbers.forEach(function(num) {
-//   if (num < 10) {
-//     newNumbers.push(num);
-//   }
-// })
+//// 필터 대신 for each 문으로
+var newNumbers = [];
+numbers.forEach(function (num) {
+  if (num < 10) {
+    newNumbers.push(num);
+  }
+});
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//   console.log("accumulator = " + accumulator);
-//   console.log("currentNumber = " + currentNumber);
-//     return accumulator + currentNumber;
-// })
+//// forEach문으로 numbers.를 통해서 loop through array로
+//// 현재숫자를 하나씩 잡아서 new number에 추가해줌.
 
-// var newNumber = 0;
-// numbers.forEach(function (currentNumber) {
-//   newNumber += currentNumber
-// })
+var newNumbers = 0;
+numbers.forEach(function (currentNumber) {
+  newNumbers += currentNumber;
+});
+
+//// Reduce Function의 사용
+var newNumbers = numbers.reduce(function (accumulator, currentNumber) {
+  console.log("accumulator = " + accumulator);
+  console.log("currentNumber = " + currentNumber);
+
+  return accumulator + currentNumber;
+});
 
 //Find - find the first item that matches from an array.
-
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
-
-// console.log(newNumber);
+//// 가장 먼저 찾아지는 숫자를 잡고 끝.
+const newNumbers = numbers.find(function (num) {
+  return num > 10;
+});
 
 //FindIndex - find the index of the first item that matches.
+////
+const newNumbers = numbers.findIndex(function (num) {
+  return num > 10;
+});
 
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+console.log(newNumbers);
 
-// console.log(newNumber);
+//// emojipedia practice
 
-//Challenge Solution
 import emojipedia from "./emojipedia";
 
-const newEmojipedia = emojipedia.map(function(emojiEntry) {
+const newEmojipedia = emojipedia.map(function (emojiEntry) {
   return emojiEntry.meaning.substring(0, 100);
 });
 
