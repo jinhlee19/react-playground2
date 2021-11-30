1eb1d4fe590b61153528156331856c381015322a
rafy <jinhlee19@gmail.com>
2021-11-30 12:52:58 +0900

    conditional rendering with the Ternary Operator & AND operator

--- conditional-rendering/src/components/App.jsx
+++ conditional-rendering/src/components/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+
+const currentTime = new Date(2021, 11, 19, 6).getHours();
+console.log(currentTime);
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="container">
+			{
+				// isLoggedIn ? <h1>Hello</h1> : <Login />
+				currentTime < 12 && <h1>Why are you still working?</h1>
+			}
+		</div>
+	);
 }
 
 export default App;
