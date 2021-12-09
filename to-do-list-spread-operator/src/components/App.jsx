37440408f555208c713c906563eb664d33a2df48
rafy <jinhlee19@gmail.com>
2021-12-10 07:04:01 +0900

    Spread Operator Practice - To Do List 1

--- to-do-list-spread-operator/src/components/App.jsx
+++ to-do-list-spread-operator/src/components/App.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+//1. When new text is written into the input, its state should be saved.
+
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          <li>A Item </li>
-        </ul>
-      </div>
-    </div>
-  );
+
+	const [inputText, setInputText] = useState([]);
+	const [items, setItems] = useState([]);
+
+	function handleChange(event) {
+		const newValue = event.target.value;
+		setInputText(newValue);
+	}
+	return (
+		<div className="container">
+			<div className="heading">
+				<h1>To-Do List</h1>
+			</div>
+			<div className="form">
+				<input onChange={handleChange} type="text" value={inputText} />
+				<button>
+					<span>Add</span>
+				</button>
+			</div>
+			<div>
+				<ul>
+					{items.map((todoItem) => (
+						<li>{todoItem}</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
+
+// {items.map(todoItem => {
+// 	return <li>{todoItem}</li>;
+// 	})}
