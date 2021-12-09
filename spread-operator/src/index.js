09ec783a5a7c7307f2315fb9405c24c660e9024d
rafy <jinhlee19@gmail.com>
2021-12-10 02:55:51 +0900

    Spread Operator 1

--- spread-operator/src/index.js
+++ spread-operator/src/index.js
@@ -3,3 +3,23 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const citrus = ["Lime", "Lemon", "Orange"];
+const fruits = ["Apple", ...citrus, "Banana", "Coconut", ];
+
+console.log(fruits);
+
+const fullName = {
+    fName: "James" ,
+    lName: "Bond"
+}
+
+const user = {
+    ...fullName, 
+    // fullName으로 그냥 넣으면 object로 묶여서 들어가지만
+    // 스프레드 오퍼레이터 '...'사용시 퍼뜨려서 들어가주는 역할
+    id: 1,
+    username: "jamesbond007"
+};
+
+console.log(user);
